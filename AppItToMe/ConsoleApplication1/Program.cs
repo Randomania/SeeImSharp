c5804a271fa9096d0e2c730d9766c21210d56514
Randomania <inobliss@hotmail.com>
2015-05-26 13:59:45 -0500

    Sup

--- AppItToMe/ConsoleApplication1/Program.cs
+++ AppItToMe/ConsoleApplication1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,34 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hey");
+            Console.WriteLine("This is a calculator");
+            int num1, num2, total = 0;
+            char symbol;
+
+           // num1 = Convert.ToInt32(Console.ReadLine());
+           // symbol = Convert.ToChar(Console.ReadLine());
+            //num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (File.Exists("Salami.txt"))
+            {
+                Console.WriteLine("Oh het its here, great");
+            }
+            MailMessage mail = new MailMessage("you@yourcompany.com", "user@hotmail.com");
+            SmtpClient client = new SmtpClient();
+            /*switch (symbol)
+            {
+                case '+':
+                    {
+                        total = num1 + num2;
+                        Console.WriteLine("Okay");
+                        break;
+                    }
+
+            }
+
+            Console.WriteLine("The answer is " + total);*/
+            Console.ReadKey();
+
         }
     }
 }
